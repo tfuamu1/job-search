d2baca276f4ea55535aec2bc8f88c726da85adf4
thuyphamthu <thuyphamthu1509@gmail.com>
2023-12-26 16:40:07 +0900

    Removing unnecessary elements

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,3 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div class="wrapper">
-    <h1>Hello, I'm in App.vue</h1>
-    <HelloWorld msg="You did it!" />
-  </div>
+  <h1>Hello Vue</h1>
 </template>
-
-<style scoped>
-h1 {
-  color: purple;
-}
-</style>
